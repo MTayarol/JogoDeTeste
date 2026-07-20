b944f218f342d002a4b500b34bb430149cd057ea
MTayarol <tayarolmacatay@gmail.com>
2026-07-20 17:28:58 -0300

    adição de movimento e algumas animações de transição

--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -6,15 +6,24 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float movementSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+
+    private Animator animator;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.linearVelocity = moveInput * movementSpeed;
+        if (rb.linearVelocity != Vector2.zero){
+            animator.SetInteger("transicao", 1);
+        }else{
+            animator.SetInteger("transicao", 0);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
