089e131e1562256d1ca50d7daa631ccdb1bb4b68
MTayarol <tayarolmacatay@gmail.com>
2026-08-03 17:12:16 -0300

    Interface de vida, atualização de unity hub

--- Assets/Scenes/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scenes/Scripts/PlayerScripts/PlayerHealth.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int currentHealth = 3;
     public int maxHealth = 3;
+
+    public TMP_Text healthText;
+
+    private void Start()
+    {
+        healthText.text = "Hp: " + currentHealth + " / " + maxHealth;
+    }
     
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
+        healthText.text = "Hp: " + currentHealth + " / " + maxHealth;
         if(currentHealth <=0)
         {
             gameObject.SetActive(false);
